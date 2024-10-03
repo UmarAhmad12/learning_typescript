c75698896c17e130cc7f9d7fe8745d61a927f70e
UmarAhmad12 <umara6274@gmail.com>
2024-10-04 01:52:00 +0530

    Completed useState hook

--- src/SignIn.tsx
+++ src/SignIn.tsx
@@ -2,19 +2,32 @@ import { useState } from "react"
 
 
 function SignIn() {
-    const [isSignIn, setSignIn] = useState(false);
+    // const [isSignIn, setSignIn] = useState(false);
+    interface UserType {
+      name:string,
+      email:string
+    }
+    const [user, setUser] = useState<UserType | null>(null);
+
     const handleSignIn=()=>{
-        setSignIn(true)
+        // setSignIn(true)
+        setUser({
+          name:"Umar",
+          email:"umar@gmail.com"
+        })
     }
     const handleSignOut =()=>{
         // setSignIn(0)  // this will give error because we are using typescript with react if we use plain js with react then it will not show any error.
-        setSignIn(false)
+        // setSignIn(false)
+        setUser(null);
     }
   return (
     <div>
         <button onClick={handleSignIn}>Sign In</button>
         <button onClick={handleSignOut}>Sign Out</button>
-        <div>{isSignIn?"Sign In":"Sign Out"}</div>
+        {/* <div>{isSignIn?"Sign In":"Sign Out"}</div> */}
+        <div>{user?.name}</div>
+        <div>{user?.email}</div>
     </div>
   )
 }
